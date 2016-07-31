ddfdc37504be68345a9a76729198a27350f18e00
akata in mac book air <akata.onen@gmail.com>
2016-07-31 17:01:12 +0900

    Add function to set user ID for Hatebu search

--- workflow/main.rb
+++ workflow/main.rb
@@ -5,38 +5,13 @@ require 'rubygems' unless defined? Gem # rubygems is only needed in 1.8
 require_relative "bundle/bundler/setup"
 require "alfred"
 
-
-
-
 Alfred.with_friendly_error do |alfred|
   fb = alfred.feedback
 
-  # add a file feedback
-  fb.add_file_item(File.expand_path "~/Applications/")
-
-  # add an arbitrary feedback
   fb.add_item({
-    :uid      => ""                     ,
-    :title    => "Just a Test"          ,
-    :subtitle => "feedback item"        ,
-    :arg      => "A test feedback Item" ,
-    :valid    => "yes"                  ,
+    :title => "User ID: #{ARGV[0]}" ,
+    :arg   => ARGV[0]               , # This is the value that is passed to the next portion
   })
-  
-  # add an feedback to test rescue feedback
-  fb.add_item({
-    :uid          => ""                     ,
-    :title        => "Rescue Feedback Test" ,
-    :subtitle     => "rescue feedback item" ,
-    :arg          => ""                     ,
-    :autocomplete => "failed"               ,
-    :valid        => "no"                   ,
-  })
-
-  if ARGV[0].eql? "failed"
-    alfred.with_rescue_feedback = true
-    raise Alfred::NoBundleIDError, "Wrong Bundle ID Test!"
-  end
 
   puts fb.to_xml(ARGV)
 end
